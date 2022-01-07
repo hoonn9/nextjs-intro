9783257e13a71e434436d416dd6e0990338aec0a
hoonn9 <hun9935@gmail.com>
2022-01-07 23:55:35 +0900

    Style navbar

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,31 @@
+import { useEffect, useState } from "react";
 import Seo from "../components/Seo";
 
+const API_KEY = "apikey";
+
 export default function Home() {
+  const [movies, setMovies] = useState([]);
+
+  async function getMovies() {
+    const { results } = await (await fetch(`https://api.themoviedb.org/3/movie/popular&api_key=${API_KEY}`)).json();
+    setMovies(results);
+  }
+
+  useEffect(() => {
+    getMovies();
+  }, []);
+
   return (
     <div>
       <Seo title="Home" />
-      <h1>Hello</h1>
+      {!movies && <h4>Loading...</h4>}
+      {movies?.map((movie) => {
+        return (
+          <div key={movie.id}>
+            <h4>{movie.original_title}</h4>
+          </div>
+        );
+      })}
     </div>
   );
 }
